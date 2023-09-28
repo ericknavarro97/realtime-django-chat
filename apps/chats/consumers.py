ad7c88d708c3545ede78c91eb3571d4fcd43a7c7
Erick Navarro <thenavarro97@gmail.com>
2023-09-27 18:45:04 -0600

    Refactor chat consumers and add custom exception handling

--- apps/chats/consumers.py
+++ apps/chats/consumers.py
@@ -5,6 +5,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.cache import cache
 from django.utils import timezone
 
+from apps.chats.exceptions import ChatConsumerException
 from services.redis import RedisService
 
 
@@ -19,8 +20,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self._chat_group_name = ''
         self.redis_service = RedisService()
 
-    async def connect(self):
-        # Get chat name from query string
+    async def connect(self) -> None:
+        """
+        Connects the websocket connection.
+
+        :return: None
+        """
+        # Get slug from query string
         self._chat_name = self.scope['url_route']['kwargs']['chat_name']
         self._chat_group_name = f'chat_{self._chat_name}'
 
@@ -34,61 +40,95 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-    async def disconnect(self, close_code):
+    async def disconnect(self, close_code) -> None:
+        """
+        Disconnects the websocket connection.
+
+        :param close_code: The close code received from the client indicating the reason for disconnecting.
+        :return: None
+        """
         self._logger.error(f'Disconnecting from {self._chat_group_name}')
         await self.channel_layer.group_discard(
             self._chat_group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        username = text_data_json['username']
-        chat = text_data_json['chat']
+    async def receive(self, text_data: str) -> None:
+        """
+        Receive s a message from the websocket connection.
 
-        # Generate a cache key for the user's message rate limit
-        cache_key = f'message_limit:{chat}:{username}'
+        :param text_data: The raw text data received from the websocket connection.
+        :return: None
+        """
+        try:
+            text_data_json = json.loads(text_data)
+            message = text_data_json['message']
+            username = text_data_json['username']
+            chat = text_data_json['chat']
 
-        # Check if the user has reached the message limit within the time frame
-        message_count = cache.get(cache_key, 0)
-        current_time = timezone.now()
-        last_message_time = cache.get(f'last_message_time:{chat}:{username}')
+            # Generate a cache key for the user's message rate limit
+            cache_key = f'message_limit:{chat}:{username}'
 
-        if last_message_time and (current_time - last_message_time).seconds < self._MESSAGE_CACHE_TIMEOUT:
-            # User sent a message too quickly, reject it
-            self._logger.error('Rate limit exceeded. Message rejected.')
-            return
+            # Check if the user has reached the message limit within the time frame
+            message_count = cache.get(cache_key, 0)
+            current_time = timezone.now()
+            last_message_time = cache.get(f'last_message_time:{chat}:{username}')
 
-        if message_count >= self._MAX_MESSAGES_PER_MINUTE:
-            # User has exceeded the message limit, reject it
-            self._logger.error('Rate limit exceeded. Message rejected.')
-            return
+            if message.strip():
+                if last_message_time and (current_time - last_message_time).seconds < self._MESSAGE_CACHE_TIMEOUT:
+                    # User sent a message too quickly, reject it
+                    self._logger.error('Rate limit exceeded. Message rejected.')
+                    raise ChatConsumerException(
+                        message=f'Rate limit exceeded. Message rejected. Max 1 message by {self._MESSAGE_CACHE_TIMEOUT} seconds'
+                    )
 
-        if message.strip():
-            # Increment the message count and set the last message time
-            cache.set(cache_key, message_count + 1, self._MESSAGE_CACHE_TIMEOUT)
-            cache.set(f'last_message_time:{chat}:{username}', current_time, self._MESSAGE_CACHE_TIMEOUT)
+                if message_count >= self._MAX_MESSAGES_PER_MINUTE:
+                    # User has exceeded the message limit, reject it
+                    self._logger.error('Rate limit exceeded. Message rejected.')
+                    raise ChatConsumerException(
+                        message=f'Rate limit exceeded. Message rejected., Max {self._MAX_MESSAGES_PER_MINUTE} per minute'
+                    )
 
-            self._logger.error(f'Data received: {text_data_json}')
+                # Increment the message count and set the last message time
+                cache.set(cache_key, message_count + 1, self._MESSAGE_CACHE_TIMEOUT)
+                cache.set(f'last_message_time:{chat}:{username}', current_time, self._MESSAGE_CACHE_TIMEOUT)
 
-            # Store the message in Redis temporarily
-            await self.redis_service.store_message_in_redis(chat_slug=chat, username=username, message=message)
+                self._logger.error(f'Data received: {text_data_json}')
 
-            # Send message to chat group
-            await self.channel_layer.group_send(
-                self._chat_group_name,
-                {
-                    'type': 'chat_message',
-                    'message': message,
-                    'username': username,
-                    'chat': chat,
-                }
-            )
-        else:
-            self._logger.error('Received an empty message')
+                # Store the message in Redis temporarily
+                await self.redis_service.store_message_in_redis(chat_slug=chat, username=username, message=message)
+
+                # Send message to chat group
+                await self.channel_layer.group_send(
+                    self._chat_group_name,
+                    {
+                        'type': 'chat_message',
+                        'message': message,
+                        'username': username,
+                        'chat': chat,
+                    }
+                )
+            else:
+                self._logger.error('Received an empty message')
+
+        except ChatConsumerException as e:
+            await self.send(text_data=json.dumps({
+                'error': e.message,
+            }))
+        except Exception as e:
+            self._logger.error("Something was wrong sending message.")
+            await self.send(text_data=json.dumps({
+                'error': 'Something was wrong sending message.',
+            }))
+            raise e
 
     async def chat_message(self, event: dict):
+        """
+        Receive a message from the chat group.
+
+        :param event: The event containing the message.
+        :return: None
+        """
         message = event['message']
         username = event['username']
         chat = event['chat']
